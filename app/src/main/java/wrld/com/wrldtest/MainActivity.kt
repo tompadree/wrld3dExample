e25b879f9847a80321cdb8f9301872b53c3c5696
Dean Jakovljevic <tomislavcuris@gmail.com>
2018-06-20 15:54:05 +0200

    pin indoor progress

--- app/src/main/java/wrld/com/wrldtest/MainActivity.kt
+++ app/src/main/java/wrld/com/wrldtest/MainActivity.kt
@@ -26,10 +26,21 @@ import com.eegeo.mapapi.services.routing.RoutingService
 import android.widget.RelativeLayout
 import com.eegeo.mapapi.services.routing.OnRoutingQueryCompletedListener
 import android.graphics.Color;
+import android.graphics.Point
+import android.graphics.PointF
+import android.widget.Button
+import com.eegeo.mapapi.positioner.PositionerOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import com.eegeo.mapapi.geometry.ElevationMode.HeightAboveGround
+import com.eegeo.mapapi.geometry.LatLngAlt
+import com.eegeo.mapapi.indoors.OnIndoorEnteredListener
+import com.eegeo.mapapi.indoors.OnIndoorExitedListener
+import com.eegeo.mapapi.positioner.OnPositionerChangedListener
+import com.eegeo.mapapi.positioner.Positioner
+import com.eegeo.ui.util.ViewAnchor
 
 
-class MainActivity : AppCompatActivity(), OnRoutingQueryCompletedListener  {
+class MainActivity : AppCompatActivity() {
 
     /*
     *
@@ -48,6 +59,8 @@ class MainActivity : AppCompatActivity(), OnRoutingQueryCompletedListener  {
     private var m_eegeoMap: EegeoMap? = null
     private val m_routeViews = ArrayList<RouteView>()
     private val m_markers = ArrayList<Marker>()
+    val markerOptions = MarkerOptions()
+    private var m_indoors = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,43 +72,53 @@ class MainActivity : AppCompatActivity(), OnRoutingQueryCompletedListener  {
         val listener = this
 
         m_mapView!!.getMapAsync { map ->
+
+
             m_eegeoMap = map
+//            m_eegeoMap!!.addOnIndoorEnteredListener(this)
+//            m_eegeoMap!!.addOnIndoorExitedListener(this)
+
+            val buttons = ArrayList<Button>()
+
+            buttons.add(findViewById(R.id.topfloor_indoor_button))
+            buttons.add(findViewById(R.id.moveup_indoor_button));
+            buttons.add(findViewById(R.id.movedown_indoor_button));
+            buttons.add(findViewById(R.id.bottomfloor_indoor_button));
+            buttons.add(findViewById(R.id.move_floor_exit_indoor_button));
+
+            val indoorEventListener = IndoorEventListener(buttons);
+            map.addOnIndoorEnteredListener(indoorEventListener)
+            map.addOnIndoorExitedListener(indoorEventListener)
 
             val uiContainer = findViewById<View>(R.id.eegeo_ui_container) as RelativeLayout
             m_indoorMapView = IndoorMapView(m_mapView!!, uiContainer, m_eegeoMap)
+            //markerOptions?.indoor(step.indoorId, step.indoorFloorId)
+//            markerOptions.position(LatLng(37.769868, -122.466106))
+////            markerOptions.indoor("Academy Cafe", 1)
+//            val marker_m = m_eegeoMap!!.addMarker(markerOptions)
+//            m_markers.add(marker_m)
 
-            val routingService = map.createRoutingService()
+            val numberOfFloors = 4
+            for (floorId in 0 until numberOfFloors) {
+                val marker = m_eegeoMap!!.addMarker(MarkerOptions()
+                        .position(LatLng(37.769868, -122.466106))
+                        .indoor("california_academy_of_sciences", floorId)
+                        .labelText("Marker on floor $floorId"))
 
-            routingService.findRoutes(RoutingQueryOptions()
-                    .addIndoorWaypoint(LatLng(56.461231653264029, -2.983122836389253), 2)
-                    .addIndoorWaypoint(LatLng(56.4600344, -2.9783117), 2)
-                    .onRoutingQueryCompletedListener(listener))
-        }
-    }
-
-    override fun onRoutingQueryCompleted(query: RoutingQuery, response: RoutingQueryResponse) {
-        Toast.makeText(this, "Found routes", Toast.LENGTH_LONG).show()
-
-        for (route in response.results) {
-            val options = RouteViewOptions()
-                    .color(Color.argb(128, 255, 0, 0))
-                    .width(8.0f)
-            val routeView = RouteView(m_eegeoMap, route, options)
-            m_routeViews.add(routeView)
-
-            for (section in route.sections) {
-                for (step in section.steps) {
-                    val markerOptions = MarkerOptions().position(step.path[0])
-                    if (step.isIndoors) {
-                        markerOptions.indoor(step.indoorId, step.indoorFloorId)
-                    }
-                    val marker = m_eegeoMap!!.addMarker(markerOptions)
-                    m_markers.add(marker)
-                }
+                m_markers.add(marker)
             }
 
+//            m_eegeoMap.addMarker(markerOptions!!)
+
+
         }
     }
+
+//    fun onClick(View view) {
+//        if (m_indoors) {
+//            m_eegeoMap.exitIndoorMap();
+//        }
+//    }
 
     override fun onResume() {
         super.onResume()
@@ -121,4 +144,64 @@ class MainActivity : AppCompatActivity(), OnRoutingQueryCompletedListener  {
 
         m_mapView!!.onDestroy()
     }
+
+    fun onExit(view: View) {
+        m_eegeoMap!!.onExitIndoorClicked()
+    }
+
+    fun onTopFloor(view: View) {
+        val indoorMap = m_eegeoMap!!.getActiveIndoorMap()
+        m_eegeoMap!!.setIndoorFloor(indoorMap.floorCount - 1)
+    }
+
+    fun onBottomFloor(view: View) {
+        m_eegeoMap!!.setIndoorFloor(0)
+    }
+
+    fun onMoveUp(view: View) {
+        m_eegeoMap!!.moveIndoorUp()
+    }
+
+    fun onMoveDown(view: View) {
+        m_eegeoMap!!.moveIndoorDown()
+    }
+
+    class IndoorEventListener(buttons: List<Button>) : OnIndoorEnteredListener, OnIndoorExitedListener {
+
+        private var m_buttons: List<Button> = buttons // ArrayList()
+//        m_buttons = buttons
+
+        override fun onIndoorEntered() {
+            setButtonStates(true)
+        }
+
+        override fun onIndoorExited() {
+            setButtonStates(false)
+        }
+
+
+        fun setButtonStates(state: Boolean) {
+            val i: Iterator<Button> = m_buttons.listIterator()
+
+            while (i.hasNext()) {
+                val b: Button = i.next()
+                b.isEnabled = state
+            }
+        }
+    }
+
+
+//    inner class IndoorEventListener internal constructor(private val m_button: Button) : OnIndoorEnteredListener, OnIndoorExitedListener {
+//
+//        override fun onIndoorEntered() {
+//            m_indoors = true
+//            m_button.isEnabled = true
+//        }
+//
+//        override fun onIndoorExited() {
+//            m_indoors = false
+//            m_button.isEnabled = false
+//        }
+//    }
+
 }
